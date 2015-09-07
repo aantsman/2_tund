8c1df47e0a2e985f5dc0795f6898dd89b8cd2b69
Annika Antsman <anniant@greeny.cs.tlu.ee>
2015-09-07 15:28:15 +0300

    errorid

--- user_form.php
+++ user_form.php
@@ -1,9 +1,30 @@
 <?php
 //"" vahele input elemendi NAME
-	echo $_POST ["email"];
-	echo $_POST ["password"];
+	//echo $_POST ["email"];
+	//echo $_POST ["password"];
 	
-
+	$email_error= "";
+	
+     
+//kontrolli ainult siis kui kasutaja vajutab login nuppu.
+	if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+		//kontrollime kasutaja emaili, et see pole tühi.
+			if(empty($_POST ["email"])) {
+				$email_error= "See on kohustuslik väli";
+			}
+	}
+	
+	$password_error="";
+	
+	if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+		//kontrollime kasutaja parooli, et see pole tühi.
+			if(empty($_POST ["password"])) {
+				$password_error= "See on kohustuslik väli";
+			}
+	}
+	
 ?>
 
 <html>
@@ -15,8 +36,8 @@
 	<body>
 		<h2>Login</h2>
 		<form action="user_form.php" method="post">
-		<input name="email" type="text" placeholder="E-mail"> <br><br>
-		<input name="password" type="password" placeholder="password"><br><br>
+		<input name="email" type="text" placeholder="E-mail"> <?php echo $email_error; ?> <br><br>
+		<input name="password" type="password" placeholder="password"> <?php echo $password_error; ?><br><br>
 		
 		<input type="submit" value="Log in">
 		</form>
