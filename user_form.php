a3eae84d1c79a5744cfdb2ba2e8887c692b4c1f7
Annika Antsman <anniant@greeny.cs.tlu.ee>
2015-09-07 14:57:34 +0300

    kasutajaliides

---
+++ user_form.php
@@ -0,0 +1,23 @@
+<?php
+
+	
+
+?>
+
+<html>
+	<head>
+		<title>User login page</title> <!--tabi nimi-->
+		
+	</head>
+
+	<body>
+		<h2>Login</h2>
+		<input type="text" placeholder="E-mail"> <br><br>
+		<input type="password" placeholder="password">
+		<br><br>
+		<input type="submit" value="Log in">
+		
+		<h2>Create user</h2>
+		
+	</body>
+</html>
