0e0ec5a773d31f83d65face87e94b66aff54dd5a
Annika Antsman <anniant@greeny.cs.tlu.ee>
2015-09-07 13:46:31 +0300

    php2

--- helloworld.php
+++ helloworld.php
@@ -12,8 +12,14 @@
 <br>
 
 <?php
-
-	$age=5;
-
-
+// 8 on >= 6 ehk 8 on suurem või võrdne kuuega. 
+// ==  võrdub
+// =! ei võrdu
+	$age=19;
+//if(loogikatehe){tõene} else{väär}
+	if($age>=18){
+		echo"täisealine";
+	} else {
+		echo "alaealine";
+	} 
 ?>
